fa81c8ed0646adac919bb2fb8944895e1db5bf1f
sreenadhs <sree2002cr@gmail.com>
2024-04-19 10:35:55 +0530

    add

--- ex.js
+++ ex.js
@@ -1,3 +1,19 @@
-let a = "234";
-console.log(a.split("")[0]-(-a.split("")[a.split("").length-1]));
-let b = "helo";
+const prompt = require('prompt-sync')();
+let b = [3,5,3,3,5],count=1;
+let c = [];
+for(let i=0;i<b.length-1;i++){
+    count = 1;
+    for(let j=i+1;j<b.length;j++){
+        if(b[i] == b[j]){
+            count++;
+            c[j] = 0;
+        }
+    }
+    if(c[i] != 0){
+       c[i] = count;
+    }
+}
+for(let i=0;i<b.length;i++){
+    console.log(`${b[i]} - ${c[i]}`); 
+}
+
